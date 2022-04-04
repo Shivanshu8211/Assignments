99d69d64254c7ee4e1e4cc7d05db1103df549625
Shivanshu8211 <102607117+Shivanshu8211@users.noreply.github.com>
2022-04-04 20:11:10 +0530

    Update Main.c

--- Assignment-1/Code/Main.c
+++ Assignment-1/Code/Main.c
@@ -1,1 +1,11 @@
-#
+#include<stdio.h>
+int main(){
+    int acctual_h;  // acctual height
+    int model_h;    // Model height
+    int acctual_v;  // Volume of the tank at the top of the acctual building
+    int model_v;    // Volume of the tank at the top of the model building
+    printf("1. Enter height of model : ");
+    scanf("%d",&model_h);
+    acctual_h=model_h*30;   // as (1/30) = model height/acctual height  ==>  acctual height= model height x 30
+    printf("Heieght of the acctual building is %d centimeter\n",acctual_h);
+
