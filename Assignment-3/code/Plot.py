e8b9f38cdf4c6fdf2b1ac1b96c8ad406f67a3416
Shivanshu8211 <102607117+Shivanshu8211@users.noreply.github.com>
2022-04-27 01:15:27 +0530

    Update Plot.py

--- Assignment-3/code/Plot.py
+++ Assignment-3/code/Plot.py
@@ -1,7 +1,35 @@
 import matplotlib.pyplot as plt
 
 # Data from question
-a=[1,1,1,1,1,2,2,2,3,3,3,5,5,5,5,5,5,7,7,7,10,10,15,15]
+"""  __________________________________________
+        Age(in years)   |  Number of children 
+            1-2         |         5
+            2-3         |         3
+            3-5         |         6
+            5-7         |         12
+            7-10        |         9
+           10-15        |         10
+           15-17        |         4
+     ___________________|_______________________
+"""
+l_val=[1,2,3,5,7,10,15]
+r_val=[2,3,5,7,10,15,17]
+freq=[5,3,6,12,9,10,4]
+"""
+Adjusted Frequency =(Frequency×Required size of interval)/Current size of interval
+Required size of interval = minimum size of interval
+"""
+# Calculating Adjusted frequency
+adjusted_freq = []
+minimum_interval = min(r_val)-min(l_val)
+for i in range(len(freq)):
+    adjusted_freq.append((freq[i]*minimum_interval)//(r_val[i]-l_val[i]))
+
+# Creating required list 'a' for plotting histogram  
+a=[]
+for i in range(len(adjusted_freq)):
+    for j in range(adjusted_freq[i]):
+        a.append((l_val[i]+r_val[i])/2)    
 
 # Lables
 plt.xlabel('No. of childrens')
@@ -9,5 +37,5 @@ plt.ylabel('Age(in years)')
 
 # Plotting graph
 plt.title('Analysis of childern of various age group playing in playground')
-plt.hist(a,bins = [1,2,3,5,7,10,15,17],facecolor='r',alpha=0.8,rwidth=0.97)
+plt.hist(a,bins = [1,2,3,5,7,10,15,17],facecolor='r',alpha=0.9,rwidth=0.97)
 plt.show()
