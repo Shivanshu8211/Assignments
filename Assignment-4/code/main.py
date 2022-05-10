bc28d0855aaae001a30ca8324ade8d0bf7381e4a
Shivanshu8211 <102607117+Shivanshu8211@users.noreply.github.com>
2022-05-10 17:28:35 +0530

    Create main.py

---
+++ Assignment-4/code/main.py
@@ -0,0 +1,48 @@
+# Given data
+S = {1,2,3,4,5,6}
+
+E = {1,3,5}
+prob_E = float(len(E)/len(S))
+
+F = {2,3}
+prob_F = float(len(F)/len(S))
+
+G = {2,3,4,5}
+prob_G = float(len(G)/len(S))
+
+prob_E_And_F = float(len(E.intersection(F))/len(S))
+
+# (i)
+# p(E/F)
+prob_E_by_F = float(prob_E_And_F/prob_F)
+print("p(E/F) = ",prob_E_by_F)
+
+# p(F/E)
+prob_F_by_E = float(prob_E_And_F/prob_E)
+print("p(F/E) = ",prob_F_by_E)
+
+prob_E_And_G = float(len(E.intersection(G))/len(S))
+
+# (ii)
+# p(E/G)
+prob_E_by_G = float(prob_E_And_G/prob_G)
+print("p(E/G) = ",prob_E_by_G)
+
+# p(G/E) 
+prob_G_by_E = float(prob_E_And_G/prob_E)
+print("p(G/E) = ",prob_G_by_E)
+
+# (iii)
+A = E.union(F)
+prob_A_and_G = float(len(A.intersection(G))/len(S))
+
+# p((E∪F)/G)
+prob_EUF_by_G = float(prob_A_and_G/prob_G)
+print("p((E∪F)/G) = ",prob_EUF_by_G)
+
+B = E.intersection(F)
+prob_B_and_G = float(len(B.intersection(G))/len(S))
+
+# p((E∩F)/G)
+prob_E_and_F_by_G = float(prob_B_and_G/prob_G)
+print("p((E∩F)/G) = ",prob_E_and_F_by_G)
