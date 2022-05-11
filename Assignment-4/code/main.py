da85bd3f4102996b9f822eb8c5ad5054c101dd03
Shivanshu8211 <102607117+Shivanshu8211@users.noreply.github.com>
2022-05-11 17:09:48 +0530

    Update main.py

--- Assignment-4/code/main.py
+++ Assignment-4/code/main.py
@@ -10,7 +10,7 @@ prob_F = float(len(F)/len(S))
 G = {2,3,4,5}
 prob_G = float(len(G)/len(S))
 
-prob_E_And_F = float(len(E.intersection(F))/len(S))
+prob_E_And_F = float(len(E and F)/len(S))
 
 # (i)
 # p(E/F)
@@ -21,7 +21,7 @@ print("p(E/F) = ",prob_E_by_F)
 prob_F_by_E = float(prob_E_And_F/prob_E)
 print("p(F/E) = ",prob_F_by_E)
 
-prob_E_And_G = float(len(E.intersection(G))/len(S))
+prob_E_And_G = float(len(E and G)/len(S))
 
 # (ii)
 # p(E/G)
@@ -33,15 +33,15 @@ prob_G_by_E = float(prob_E_And_G/prob_E)
 print("p(G/E) = ",prob_G_by_E)
 
 # (iii)
-A = E.union(F)
-prob_A_and_G = float(len(A.intersection(G))/len(S))
+A = E or F
+prob_A_and_G = float(len(A and G)/len(S))
 
 # p((E∪F)/G)
 prob_EUF_by_G = float(prob_A_and_G/prob_G)
 print("p((E∪F)/G) = ",prob_EUF_by_G)
 
-B = E.intersection(F)
-prob_B_and_G = float(len(B.intersection(G))/len(S))
+B = E and F
+prob_B_and_G = float(len(B and G)/len(S))
 
 # p((E∩F)/G)
 prob_E_and_F_by_G = float(prob_B_and_G/prob_G)
