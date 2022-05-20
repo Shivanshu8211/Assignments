dccd43f1c8ee9a365defa6d15597565a37d5a8d1
Shivanshu8211 <102607117+Shivanshu8211@users.noreply.github.com>
2022-05-20 12:51:24 +0530

    Create main.py

---
+++ Assignment-5/code/main.py
@@ -0,0 +1,28 @@
+# Sample space
+S = {1,2,3,4}
+
+# Given sets
+A = {1}
+B = {2,3}
+
+# Smallest field must has these sets
+# 1. Union
+U_set = (A | B)
+
+# 2. Intersection
+I_set = (A & B)
+
+# 3. Complement
+C_set_1 = (S - A)
+C_set_2 = (S - B)
+
+# 4. Empty set
+E_set = {}
+
+# 5. Sample space
+S_set = S
+
+print("The elements of the require set are:",A,B,U_set,E_set,C_set_1,C_set_2,S_set) 
+
+
+
